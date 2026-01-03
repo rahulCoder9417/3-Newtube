5706cef6b899915a27272fca3c0e2f3afc3edede
Rahul Kumar <rahulkum94l94@gmail.com>
2026-01-03 18:10:36 +0530

    made changes

--- beckend/src/controllers/chat.controller.js
+++ beckend/src/controllers/chat.controller.js
@@ -1,6 +1,6 @@
 import { Chat } from "../models/chat.model.js";
-import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
 
 
